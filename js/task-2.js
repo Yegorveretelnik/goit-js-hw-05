79cb5c33cab47a69780b824ef97ddad64b7fa2b1
yegor <yegorveretelnik@gmail.com>
2024-12-29 11:17:45 +0100

    commit message

--- js/task-2.js
+++ js/task-2.js
@@ -1,8 +1,37 @@
-function getShippingMessage(country, price, deliveryFee) {
-    let finalPrice = price + deliveryFee
-    return "Shipping to " + country + " will cost " + finalPrice + " credits"
-}
 
-console.log(getShippingMessage("Australia", 120, 50)); 
-console.log(getShippingMessage("Germany", 80, 20)); 
-console.log(getShippingMessage("Sweden", 100, 20)); 
+const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName) );
+
+const allUsers = [
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"]
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"]
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"]
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"]
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"]
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"]
+    }
+  ];
+  
+  console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+  console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+  console.log(getUsersWithFriend(allUsers, "Adrian Cross" ));
